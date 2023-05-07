c7a9a77a6c13293493d70e0ffd377a93aab0123b
u8uhh <93701335+u8uhh@users.noreply.github.com>
2023-05-07 21:26:04 +0200

    Add files via upload

--- lang.js
+++ lang.js
@@ -1,35 +1,37 @@
 const langArr = {
     "unit" :  {
-        "en": "That's an organisation with helps people with somethings, like residence, automobile and job. Can I get financial aid here? If you live in Ukraine and do not have money to take care of yourself and your family, the Jobcenter may be the right point of contact for you. Under certain conditions, the job center provides assistance to people who have never worked in Ukraine, have been unemployed for more than a year, or earn little money despite working. In the employment center, you can get financial support for yourself and your family. This benefit is officially called Citizen money. It is a successor to unemployment benefit II (unemployment benefit II). Employees of the employment center will support you in finding a job.",
-        "ua": "Це організація, яка допомагає людям у таких питаннях, як проживання, автомобіль і робота. Чи можу я отримати тут фінансову допомогу? Якщо ви живете в Україні і не маєте грошей, щоб подбати про себе та свою родину, центр зайнятості може стати для вас правильним місцем контакту. За певних умов центр зайнятості надає допомогу людям, які ніколи не працювали в Україні, були безробітними більше року або мало заробляють, незважаючи на роботу. У центрі зайнятості Ви можете отримати матеріальну підтримку для себе та своєї родини. Ця пільга офіційно називається Citizen money. Він є наступником допомоги по безробіттю II (допомога по безробіттю II). Співробітники центру зайнятості підтримають Вас у пошуку роботи.",
+        "en": "Das ist eine Organisation, die Menschen bei Dingen wie Wohnung, Auto und Job hilft. Bekomme ich hier finanzielle Unterstützung? Wenn Sie in der Ukraine leben und kein Geld haben, um für sich und Ihre Familie zu sorgen, ist das Jobcenter vielleicht die richtige Anlaufstelle für Sie. Das Jobcenter leistet unter bestimmten Voraussetzungen Hilfestellung für Menschen, die noch nie in der Ukraine gearbeitet haben, länger als ein Jahr arbeitslos sind oder trotz Arbeit nur wenig verdienen. Im Arbeitsamt erhalten Sie finanzielle Unterstützung für sich und Ihre Familie. Diese Leistung wird offiziell Bürgergeld genannt. Es ist ein Nachfolger des Arbeitslosengeldes II (Arbeitslosengeld II). Mitarbeiterinnen und Mitarbeiter des Arbeitsamtes unterstützen Sie bei der Stellensuche.",
+        "ua": "Це організація, яка допомагає людям у таких питаннях, як проживання, автомобіль і робота. Чи можу я отримати тут фінансову допомогу? Якщо ви живете в Україні і не маєте грошей, щоб подбати про себе та свою родину, центр зайнятості може стати для вас правильним місцем контакту. За певних умов центр зайнятості надає допомогу людям, які ніколи не працювали в Україні, були безробітними більше року або мало заробляють, незважаючи на роботу. У центрі зайнятості Ви можете отримати матеріальну підтримку для себе та своєї родини. Ця пільга офіційно називається Громадянські гроші. Він є наступником допомоги по безробіттю II (допомога по безробіттю II). Співробітники центру зайнятості підтримають Вас у пошуку роботи.",
+        "ge": "That's an organisation with helps people with somethings, like residence, automobile and job. Can I get financial aid here? If you live in Ukraine and do not have money to take care of yourself and your family, the Jobcenter may be the right point of contact for you. Under certain conditions, the job center provides assistance to people who have never worked in Ukraine, have been unemployed for more than a year, or earn little money despite working. In the employment center, you can get financial support for yourself and your family. This benefit is officially called Citizen money. It is a successor to unemployment benefit II (unemployment benefit II). Employees of the employment center will support you in finding a job.",
     }, 
+
+    
     "product-title": {
         "en": "Jobcenter",
         "ua": "Центр зайнятості",
+        "ge": "Jobcenter",
     }, 
     "product-title1": {
-        "en": "About Jobcenter",
+        "ge": "About Jobcenter",
         "ua": "Про центр зайнятості",
+        "en": "Über Jobcenter",
     }, 
 
     "product-title2":{
-        "en": "To another pages",
+        "ge": "To another pages",
         "ua": "До інших сторінок",
+        "en": "Zu anderen seits",
     },
 
     "product-title3":{
-        "en": "You can also contact with us!",
+        "ge": "You can also contact with us!",
         "ua": "Ви також можете зв'язатись з нами",
+        "en": "Sie können auch mit uns Kontakt aufnehmen!",
     },
 
     "more":{
-        "en": "Search residence",
+        "ge": "Search residence",
         "ua": "Знайти житло",
+        "en": "Wohnsitz suchen",
     },
-
-    "cool": {
-        "ru": "тип системы охлаждения",
-        "en": "сooling system type",
-        "ua": "тип системи охолодження",
-    }, 
 }
